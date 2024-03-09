19d78547d5a6d245d50390ab6ea39b0133c60018
gogden245 <130518421+gogden245@users.noreply.github.com>
2024-03-08 20:16:17 -0700

    Final App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './CollegeBasketballTeams1.json';
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+const teamNames: TeamProps[] = data.teams;
+
+function Welcome() {
+  return <h1>NCAA Basketball Teams</h1>;
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const { school, name, city, state } = this.props;
+
+    return (
+      <div className="team-card">
+        <h2>School: {school}</h2>
+        <h3>Mascot: {name}</h3>
+        <h3>
+          City: {city}, {state}
+        </h3>
+        <br></br>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  // this is a function
+  return (
+    <div className="team-list">
+      {teamNames.map((team, index) => (
+        <Team key={index} {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
